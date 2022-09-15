52a440004cd55a817719601846f4bb69413b2765
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-09-14 21:18:27 -0300

    feature: Adiciona Cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
 import { convertMinutesStringToHours } from "./utils/convert-minutes-string-to-hours";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient({
   log: ["query"],
